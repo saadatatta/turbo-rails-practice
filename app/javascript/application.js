da60b841569e39028e59a033fedb0d705c286395
Saadat Atta <muhammadsaadat42@gmail.com>
2023-09-04 12:13:14 +0500

    Flash messages added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+
+
+import '@hotwired/turbo-rails'
+// Turbo.session.drive = false // disable turbo app wide and add for specific elements gradually
+
 import "./controllers"
